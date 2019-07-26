980b1f28aff802498a84e6cb166589e3cc3be76f
ardasevinc <ardasevinc4@gmail.com>
2019-07-26 13:33:26 +0300

    debug mode

--- src/etch-a-sketch.js
+++ src/etch-a-sketch.js
@@ -1,6 +1,7 @@
 let gridParent = document.querySelector('.game-container')
 let gridChildren = document.querySelectorAll('.game-container div');
 let colorStat = 0;
+let highlightStat = 0;
 
 
 function generateRandomNum(end) {
@@ -16,19 +17,22 @@ function generateRandomColor() {
 
 
 function createGridChildren(num) {
+    updateChildren();
 
     for (let i=0; i<num*num; i++) {        
         gridParent.appendChild(document.createElement('div'));
     }
 
-    console.info(`${num*num} divs created`);
+    console.info(`createGridChildren() created ${num*num} grid children`);
     
 }
 
 
 function updateChildren() {
+
     gridChildren = document.querySelectorAll('.game-container div');
-    console.info(`children refreshed, ${gridChildren.length} children`);
+    
+    console.info(`updateChildren() updated ${gridChildren.length} children`);
 }
 
 
@@ -39,7 +43,7 @@ function removeGridChildren() {
         gridParent.removeChild(gridChildren[i]);
     }
 
-    console.info(`${gridChildren.length} divs removed`);
+    console.info(`removeGridChildren() removed ${gridChildren.length} children`);
 }
 
 
@@ -51,23 +55,59 @@ function resizeGridChildren(num) {
         gridChildren[i].style.width = `calc(60vw / ${num})`;
         gridChildren[i].style.height = `calc(65vh / ${num})`;
     }
-    console.info('resize complete');
+    console.info(`resizeGridChildren resized ${gridChildren.length}`);
 }
 
 
-function colorWhenHover(colorCode) {
+function colorWhenHover(colorFlag) {
 
-    if (colorCode === 0) {
+    if (colorFlag === 0) {
         gridParent.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = 'black';
-        })    
+        })
+        
+        console.info('black color mode activated');
     }
 
-    else if (colorCode === 1) {
+    else if (colorFlag === 1) {
         gridParent.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = generateRandomColor();
         })
+
+        console.info('color mode activated')
     }
+}
+
+
+function highlightWhenHover(highlightFlag) {
+    let targetColor;
+    let x=0.1;
+    let rgbString = "rgba" + "(0,0,0," + x + ')';
+    let splitRgb;
+     
+
+    if (highlightFlag) {
+        
+        gridParent.addEventListener('mouseover', (e) => {
+            targetColor = getComputedStyle(e.target).backgroundColor;    
+
+            if (targetColor === 'rgb(217, 184, 150)') {
+                e.target.style.backgroundColor = rgbString;
+
+                console.info(`color:${targetColor}`);
+            }
+            else {
+                if (targetColor[11] < 1) {
+                    x = targetColor[11] * 10;
+                    x += 1;
+                    x = x / 10;
+                    e.target.style.backgroundColor = rgbString;
+                }
+            }
+        });
+    }
+
+    
 }
 
 
@@ -85,25 +125,44 @@ function colorButtonPressed() {
 }
 
 
+function highlightButtonPressed() {
+    ++highlightStat;
+
+    if (highlightStat % 2 === 0) {
+        highlightWhenHover(0);
+    }
+
+    else if (highlightStat % 2 === 1) {
+        highlightWhenHover(1);
+    }
+}
+
+
 function getSizeFromUser() {
     let userWanted;
 
     userWanted = prompt('Grid size? (width=height, enter either)', '');
 
-    if (!(userWanted)) alert('Illegal number. Try again.');
+    if (!(userWanted)) {
+
+        alert('Illegal number. Try again.'); 
+        return 0;
+    } 
+
     else {
+        console.info(`getSizeFromUser() got the size ${Number(userWanted)}`);
         return Number(userWanted);
     }
-    return -1;
 }
 
 
-function resetGrid() {
+function cleanGrid() {
     updateChildren();
 
     for (let i=0; i<gridChildren.length; i++) {
         gridChildren[i].style.backgroundColor = 'hsl(30, 47%, 72%)';
     }
+    console.info(`cleanGrid() resetted the colors of the grid`);
 }
 
 
@@ -113,8 +172,13 @@ function changeGridSize() {
     let userWants = getSizeFromUser();
 
     removeGridChildren();
+    console.info(`changeGridSize() removed all children`);
+
     createGridChildren(userWants);
+    console.info(`changeGridSize() created ${userWants*userWants} children`);
+
     resizeGridChildren(userWants);
+    console.info(`changeGridSize() resized ${userWants*userWants} children`);
 }
 
 
@@ -122,7 +186,7 @@ function main() {
     createGridChildren(20);
     resizeGridChildren(20);
 
-    colorWhenHover(0);
+    console.info(`main() initialized the game with 400 grid children`);
 }
 
 main();
